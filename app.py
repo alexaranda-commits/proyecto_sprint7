58329b7f8aeccb1180f1986823c99cf647999592
alexaranda-commits <alex.aranda.g@gmail.com>
2025-10-29 20:13:23 -0700

    added title and loaded data

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
 import pandas as pd
 import plotly.graph_objects as go
 import streamlit as st
+
+st.title("Proyecto Sprint 7 - Análisis Exploratorio de Datos")
+st.write("Análisis exploratorio de datos del conjunto de datos de vehículos en EE.UU.")
+# Cargar los datos
+df_vehicles = pd.read_csv('vehicles_us.csv')
